8199d90d84bfa190da2d0c19483f78b3c67b1575
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2024-12-10 18:34:49 +0200

    add order info

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const orderInfo = document.getElementById('orderinfo')
 const pizzaSize = document.getElementById('itemsize')
 const prevSizeBtn = document.getElementById('prevsizebtn')
 const nextSizeBtn = document.getElementById('nextsizebtn')
@@ -8,6 +9,16 @@ const nextExtraOneBtn = document.getElementById('nextextra-1-btn')
 const prevExtraTwoBtn = document.getElementById('prevextra-2-btn')
 const nextExtraTwoBtn = document.getElementById('nextextra-2-btn')
 
+const updateOrderInfo = () => {
+	orderInfo.innerHTML =
+		'Піца ' +
+		size[currentSize] +
+		' з ' +
+		extras[currentExtraOne] +
+		' і ' +
+		extras[currentExtraTwo]
+}
+
 const size = [20, 25, 30, 35, 45]
 let currentSize = 0
 
@@ -15,6 +26,7 @@ let currentSize = 0
 const changeSize = direction => {
 	currentSize = (currentSize + direction + size.length) % size.length
 	pizzaSize.innerHTML = size[currentSize]
+	updateOrderInfo()
 }
 
 // Додаємо обробники подій
@@ -31,12 +43,14 @@ const changeExtraOne = direction => {
 	currentExtraOne =
 		(currentExtraOne + direction + extras.length) % extras.length
 	pizzaExtraOne.innerHTML = extras[currentExtraOne]
+	updateOrderInfo()
 }
 
 const changeExtraTwo = direction => {
 	currentExtraTwo =
 		(currentExtraTwo + direction + extras.length) % extras.length
 	pizzaExtraTwo.innerHTML = extras[currentExtraTwo]
+	updateOrderInfo()
 }
 
 // Додаємо обробники подій
@@ -44,3 +58,5 @@ prevExtraOneBtn.addEventListener('click', () => changeExtraOne(-1))
 nextExtraOneBtn.addEventListener('click', () => changeExtraOne(1))
 prevExtraTwoBtn.addEventListener('click', () => changeExtraTwo(-1))
 nextExtraTwoBtn.addEventListener('click', () => changeExtraTwo(1))
+
+updateOrderInfo()
